36a0df5c9ce1cc1ea76d802ce313e4b9e3b7dd9e
Guilherme Estrela Lambert de Brito <guilhermestrela@gmail.com>
2023-11-20 21:55:25 -0300

    comecando a gofigura os cards dos colaboradores

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/guiestrela.png' alt='Guilherme Estrela'/>
+            </div>
+            <div className='rodape'>
+                <h4>Guilherme Estrela</h4>
+                <h5>Front-End</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
